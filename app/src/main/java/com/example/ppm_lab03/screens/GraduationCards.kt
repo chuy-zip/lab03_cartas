aff07f1fcd9e3ed0414515b54d28348974b5ad30
chuy-zip <andreschuy03@gmail.com>
2023-08-30 13:47:27 -0600

    Added modifiable composable function

--- app/src/main/java/com/example/ppm_lab03/screens/GraduationCards.kt
+++ app/src/main/java/com/example/ppm_lab03/screens/GraduationCards.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -17,34 +18,53 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import com.example.ppm_lab03.navigation.AppScreens
 
 @Composable
 fun GraduationCards(navController: NavController, name: String?, message: String?, receiver: String?){
-
     Column(
         modifier = Modifier
             .fillMaxSize(),
         verticalArrangement = Arrangement.SpaceBetween,
         horizontalAlignment = Alignment.CenterHorizontally
     ){
-        TopTitle(tittle = "Graduacion", navController)
-        BodyContent(navController)
 
-        Text(text = "$name \n $message \n $receiver")
+        TopTitle(tittle = "Graduacion", navController)
+
+        var currentSelection by remember {mutableStateOf("1")}
+        
+        when (currentSelection) {
+            "1" -> CustomCard(name, message, receiver, 1)
+            "2" -> CustomCard(name, message, receiver, 2)
+            "3" -> CustomCard(name, message, receiver, 3)
+            "4" -> CustomCard(name, message, receiver, 4)
+        }
+
+        //BodyContent(navController)
+
+        BottomBar(currentSelection){newSelection ->
+            currentSelection = newSelection
+        }
 
     }
 
@@ -83,8 +103,73 @@ fun TopTitle(tittle: String, navController: NavController){
 }
 
 @Composable
+fun BottomBar(selection: String, onSelectionChange: (String) -> Unit){
+    Row( modifier = Modifier
+        .fillMaxWidth()
+        .height(80.dp),
+        horizontalArrangement = Arrangement.SpaceAround){
+
+        Button(
+            onClick = {onSelectionChange("1")},
+            shape = RectangleShape,
+            modifier = Modifier
+                .weight(1f)
+                .fillMaxWidth()
+                .fillMaxHeight(),
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color(51,97,172),
+                contentColor = Color.White)) {
+            Text(text = "1")
+        }
+        Button(
+            onClick = {onSelectionChange("2")},
+            shape = RectangleShape,
+            modifier = Modifier
+                .weight(1f)
+                .fillMaxWidth()
+                .fillMaxHeight(),
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color(51,97,172),
+                contentColor = Color.White)) {
+            Text(text = "2")
+        }
+        Button(
+            onClick = {onSelectionChange("3")},
+            shape = RectangleShape,
+            modifier = Modifier
+                .weight(1f)
+                .fillMaxWidth()
+                .fillMaxHeight(),
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color(51,97,172),
+                contentColor = Color.White)) {
+            Text(text = "3")
+        }
+        Button(
+            onClick = {onSelectionChange("4")},
+            shape = RectangleShape,
+            modifier = Modifier
+                .weight(1f)
+                .fillMaxWidth()
+                .fillMaxHeight(),
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color(51,97,172),
+                contentColor = Color.White)) {
+            Text(text = "4")
+        }
+
+    }
+}
+
+@Composable
 fun BodyContent(navController: NavController){
-    Column(){
+    Column(
+        modifier = Modifier
+            .height(570.dp)
+            .padding(30.dp),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
         Text("Prueba de navegacion")
         Button(onClick = {
             navController.navigate(route = AppScreens.MothersDayCards.route)
@@ -92,4 +177,18 @@ fun BodyContent(navController: NavController){
             Text(text = "Navegar...")
         }
     }
+}
+
+@Composable
+fun CustomCard(name: String?, message: String?, receiver: String?, intTest: Int){
+    Column(
+        modifier = Modifier
+            .height(570.dp)
+            .padding(30.dp),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "$name \n $message \n $receiver \n Tarjeta: $intTest")
+    }
+
 }
