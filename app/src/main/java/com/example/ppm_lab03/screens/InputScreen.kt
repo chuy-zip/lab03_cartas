9f51fc6ef5007c05bcae696913b50a6178e1ce31
Dan <pen22217@uvg.edu.gt>
2023-08-30 19:36:29 -0600

    Added Login Screen Functionality and Design

--- app/src/main/java/com/example/ppm_lab03/screens/InputScreen.kt
+++ app/src/main/java/com/example/ppm_lab03/screens/InputScreen.kt
@@ -1,15 +1,22 @@
 package com.example.ppm_lab03.screens
 
+import android.util.DisplayMetrics
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -18,30 +25,97 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.example.ppm_lab03.R
+import com.example.ppm_lab03.navigation.AppNavigation
 import com.example.ppm_lab03.navigation.AppScreens
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun InputScreen(navController: NavController){
+
+    Image(painter = painterResource(id = R.drawable.loginscreenlayout), contentDescription = "Background Image.",
+        modifier = Modifier.fillMaxSize(),
+        contentScale = ContentScale.Crop)
+
     Column (
         modifier = Modifier
             .fillMaxSize(),
-        verticalArrangement = Arrangement.SpaceEvenly,
-        horizontalAlignment = Alignment.CenterHorizontally
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceBetween
     ){
 
         var nameLastname by rememberSaveable { mutableStateOf("") }
         var receiver by rememberSaveable { mutableStateOf("") }
         var message by rememberSaveable { mutableStateOf("") }
 
+        DisplayMetrics()
+        val screenHeight = with(LocalDensity.current) {
+            LocalContext.current.resources.displayMetrics.heightPixels.toDp()
+        }
+
         var selectedItem by remember { mutableStateOf("Cumpleaños") }
         var isMenuExpanded by remember { mutableStateOf(false) }
 
+        Spacer(modifier = Modifier.height(screenHeight / 2))
+
+        TextField(
+            value = nameLastname,
+            onValueChange = { nameLastname = it},
+            label = { Text ("Remitente") },
+            singleLine = true,
+            shape = CircleShape,
+            colors = TextFieldDefaults.textFieldColors(
+                textColor = Color.White,
+                containerColor = Color(0xff8f8e8e),
+                unfocusedLabelColor = Color.White
+            )
+        )
+
+        TextField(
+            value = receiver,
+            onValueChange = { receiver = it},
+            label = { Text ("Destinatario") },
+            singleLine = true,
+            shape = CircleShape,
+            colors = TextFieldDefaults.textFieldColors(
+                textColor = Color.White,
+                containerColor = Color(0xff8f8e8e),
+                unfocusedLabelColor = Color.White
+            )
+        )
+
+        TextField(
+            value = message,
+            onValueChange = { message = it},
+            label = { Text ("Mensaje") },
+            singleLine = false,
+            shape = CircleShape,
+            colors = TextFieldDefaults.textFieldColors(
+                textColor = Color.White,
+                containerColor = Color(0xff8f8e8e),
+                unfocusedLabelColor = Color.White
+            )
+        )
+
         Column {
-            OutlinedButton(
+            Button(
                 onClick = { isMenuExpanded = true},
-                content = { Text(selectedItem)}
+                content = { Text(selectedItem)},
+                modifier = Modifier
+                    .width(280.dp)
+                    .height(50.dp),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color(0xff8f8e8e),
+
+                    )
             )
             DropdownMenu(
                 expanded = isMenuExpanded,
@@ -87,46 +161,41 @@ fun InputScreen(navController: NavController){
             }
         }
 
-        TextField(
-            value = nameLastname,
-            onValueChange = { nameLastname = it},
-            label = { Text ("Ingrese su nombre y apellido:") },
-            singleLine = true
-        )
-
-        TextField(
-            value = receiver,
-            onValueChange = { receiver = it},
-            label = { Text ("A quien va dirigida la carta:") },
-            singleLine = true
-        )
-
-        TextField(
-            value = message,
-            onValueChange = { message = it},
-            label = { Text ("Mensaje que desea enviar") },
-            singleLine = false
-        )
-
-        Button(onClick = {
-            if(selectedItem == "Cumpleaños"){
-                navController.navigate(route = AppScreens.BirthdayCards.route + "/$nameLastname" + "/$message" + "/$receiver")
-            }
-            else if(selectedItem == "Graduación"){
-                navController.navigate(route = AppScreens.GraduationCards.route + "/$nameLastname" + "/$message" + "/$receiver")
-            }
-            else if(selectedItem == "Año nuevo chino"){
-                navController.navigate(route = AppScreens.ChineseNewYearCards.route + "/$nameLastname" + "/$message" + "/$receiver")
-            }
-            else if(selectedItem == "Día de la madre"){
-                navController.navigate(route = AppScreens.MothersDayCards.route + "/$nameLastname" + "/$message" + "/$receiver")
-            }
-            else if(selectedItem == "Día del padre"){
-                navController.navigate(route = AppScreens.FathersDayCards.route + "/$nameLastname" + "/$message" + "/$receiver")
-
-            }
+        Button(
+            modifier = Modifier
+                .width(280.dp)
+                .height(50.dp),
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color(0xffff5757)
+            ),
+            onClick = {
+                when (selectedItem) {
+                    "Cumpleaños" -> {
+                        navController.navigate(route = AppScreens.BirthdayCards.route + "/$nameLastname" + "/$message" + "/$receiver")
+                    }
+                    "Graduación" -> {
+                        navController.navigate(route = AppScreens.GraduationCards.route + "/$nameLastname" + "/$message" + "/$receiver")
+                    }
+                    "Año nuevo chino" -> {
+                        navController.navigate(route = AppScreens.ChineseNewYearCards.route + "/$nameLastname" + "/$message" + "/$receiver")
+                    }
+                    "Día de la madre" -> {
+                        navController.navigate(route = AppScreens.MothersDayCards.route + "/$nameLastname" + "/$message" + "/$receiver")
+                    }
+                    "Día del padre" -> {
+                        navController.navigate(route = AppScreens.FathersDayCards.route + "/$nameLastname" + "/$message" + "/$receiver")
+                    }
+                }
+                
         }) {
             Text(text = "Continuar")
+
         }
     }
+}
+
+@Preview
+@Composable
+fun InputPreview() {
+    AppNavigation()
 }
