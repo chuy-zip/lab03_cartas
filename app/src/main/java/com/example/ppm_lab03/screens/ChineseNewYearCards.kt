ae1dcc64993cb0e051498ec6fff4a9d55fe3fd94
Dan <pen22217@uvg.edu.gt>
2023-08-30 21:08:49 -0600

    Added Chinese New Year Layout

--- app/src/main/java/com/example/ppm_lab03/screens/ChineseNewYearCards.kt
+++ app/src/main/java/com/example/ppm_lab03/screens/ChineseNewYearCards.kt
@@ -1,5 +1,6 @@
 package com.example.ppm_lab03.screens
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -10,14 +11,20 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavController
+import com.example.ppm_lab03.R
 
 @Composable
 fun ChineseNewYearCards(navController: NavController, name: String?, message: String?, receiver: String?){
+
+    var userCard = "1"
+
     Column(
         modifier = Modifier
             .fillMaxSize(),
-        verticalArrangement = Arrangement.SpaceBetween,
+        verticalArrangement = Arrangement.Top,
         horizontalAlignment = Alignment.CenterHorizontally
     ){
 
@@ -25,15 +32,51 @@ fun ChineseNewYearCards(navController: NavController, name: String?, message: St
 
         var currentSelection by remember { mutableStateOf("1") }
 
-        when (currentSelection) {
-            "1" -> CustomCard(name, message, receiver, 1)
-            "2" -> CustomCard(name, message, receiver, 2)
-            "3" -> CustomCard(name, message, receiver, 3)
-            "4" -> CustomCard(name, message, receiver, 4)
-
+        Column {
+            when (currentSelection) {
+                "1" -> CustomCard(name, message, receiver, 1)
+                "2" -> CustomCard(name, message, receiver, 2)
+                "3" -> CustomCard(name, message, receiver, 3)
+                "4" -> CustomCard(name, message, receiver, 4)
+            }
+            userCard = currentSelection
         }
 
-        BottomBar(currentSelection){newSelection -> currentSelection = newSelection }
-
+        BottomBar(currentSelection){newSelection -> 
+            currentSelection = newSelection 
+        }
     }
+    ChineseLayout(pickedCard = userCard)
+}
+
+@Composable
+fun ChineseLayout(pickedCard: String) {
+    when (pickedCard) {
+        "1" -> {
+            Image(painter = painterResource(id = R.drawable.chinese1),
+                contentDescription = "First Chinese Card.")
+        }
+
+        "2" -> {
+            Image(painter = painterResource(id = R.drawable.chinese2),
+                contentDescription = "Second Chinese Card.")
+        }
+
+        "3" -> {
+            Image(painter = painterResource(id = R.drawable.chinese3),
+                contentDescription = "Third Chinese Card.")
+        }
+
+        "4" -> {
+            Image(painter = painterResource(id = R.drawable.chinese4),
+                contentDescription = "Fourth Chinese Card.")
+        }
+    }
+
+}
+
+@Preview
+@Composable
+fun ChinesePreview() {
+    ChineseLayout("1")
 }
